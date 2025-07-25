419a96074190e6e46666ae36464200b409cfa4bd
paulinaa333 <piramosbobatto@escuelasproa.edu.ar>
2025-07-25 15:46:47 -0300

    caja de colores

---
+++ EjerciciosEventos/JavaScript.js
@@ -0,0 +1,11 @@
+document.getElementById("Botonrojo").addEventListener("click", function(){
+       document.getElementById("caja").style.backgroundColor = "red";
+});
+
+document.getElementById("Botonverde").addEventListener("click", function(){
+        document.getElementById("caja").style.backgroundColor = "green";
+});
+
+document.getElementById("Botonazul").addEventListener("click", function(){
+        document.getElementById("caja").style.backgroundColor = "blue";
+});
